79acca0fcf3f79e53996f55adb9e2259bbb1ef95
Nastya Mayorova <77802875+mnasty125@users.noreply.github.com>
2022-02-19 00:30:06 +0300

    Adaptive and add modal check

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 // Модальное окно на главной странице
-$(".popup-fade__popup-close").click( () => {
+$(".popup-fade__popup-close").click(() => {
   $(".popup-fade").addClass("hidden");
 });
 
-$(".main__button-new").click( () => {
+$(".main__button-new").click(() => {
   $(".popup-fade").removeClass("hidden");
 });
 
 $(window).keydown((event) => {
-  if (event.key == "Escape"){
+  if (event.key == "Escape") {
     $(".popup-fade").addClass("hidden");
   }
 });
 
+// Модальное окно на check странице
+$(".check-popup-fade__popup-close").click(() => {
+  $(".check-popup-fade").addClass("hidden");
+});
+
+$(".check-main__btn-generation").click(() => {
+  $(".check-popup-fade").removeClass("hidden");
+});
+
+$(window).keydown((event) => {
+  if (event.key == "Escape") {
+    $(".check-popup-fade").addClass("hidden");
+  }
+});
+
 // Окно справки
-$("#btn-info").click( () => {
+$("#btn-info").click(() => {
   $(".check-main__info-page").toggleClass("hidden");
 });
